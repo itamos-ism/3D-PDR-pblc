b80df41f0b1fe92d8b922583129ac84b613ba814
Thomas Bisbas <tbisbas@gmail.com>
2025-11-06 16:32:55 +0800

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -6,7 +6,7 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'ITAMOS-ISM'
+project = '3D-PDR'
 copyright = '2025, Thomas G. Bisbas'
 author = 'Thomas G. Bisbas'
 
